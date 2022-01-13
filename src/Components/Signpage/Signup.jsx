62f7a8d30bd66df5593ed8907a94245a62cce640
Jayant <jayantsingh331@gmail.com>
2022-01-14 01:03:12 +0530

    mv

--- src/Components/Signpage/Signup.jsx
+++ src/Components/Signpage/Signup.jsx
@@ -98,7 +98,7 @@ export default function Signup() {
                 Paasword
               </label>
               <input
-                className="signup-input signup-password "
+                className="signup-input signup-password eyepassbg "
                 type="password"
                 placeholder=""
               />
