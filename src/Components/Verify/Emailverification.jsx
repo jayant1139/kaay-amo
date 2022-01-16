491c472a63fca1ecc4d78727b5db1d1bf8cb7f70
Jayant <jayantsingh331@gmail.com>
2022-01-16 23:13:12 +0530

    bv

--- src/Components/Verify/Emailverification.jsx
+++ src/Components/Verify/Emailverification.jsx
@@ -25,16 +25,7 @@ export default function Emailverification() {
                 <b>Verify your e-mail</b>
               </h2>
               <p className="m-3" style={colorgrey}>Hi! rohan! We have sent a code to your email id at xyz***@gmail.com</p>
-              {/* <div className="d-flex m-1">
-                <input className="otp-field" type="text" maxLength={1} />
-                <input className="otp-field" type="text" maxLength={1} />
-                <input className="otp-field" type="text" maxLength={1} />
-                <input className="otp-field" type="text" maxLength={1} />
-              </div> */}
-              {/* <div className="timer color-orange text-center">00:35</div> */}
-              {/* <a href="/kaay-amo">
-              <p className="m-3" style={colorgrey}>Send the code again</p>
-              </a> */}
+              
               <button className="verify-btn " onClick={homeredirect}>Verify</button>
               <p className='text-center'>Questions? Email us at <span className='color-orange'>kaayamo@gmail.com</span></p>
             </div>
